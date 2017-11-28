93d1741636d4ebb2e1a17b549bca868d974a26e9
tsvetos <seve0955@gmail.com>
2017-11-28 08:58:43 +0100

    Añadido CGtk

---
+++ CGtk/CGtk/MainWindow.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow: Gtk.Window
+{
+	public MainWindow () : base (Gtk.WindowType.Toplevel)
+	{
+		Build ();
+
+		buttonAceptar.Clicked += delegate {
+			Button button = new Button();
+			button.Label = " Boton creado ";
+			button.Visible = true;
+			vbox1.Add(button);
+			//labelSaludo.Text = "Hola " + entryNombre.Text;
+		};
+	}
+
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+}
+
+
