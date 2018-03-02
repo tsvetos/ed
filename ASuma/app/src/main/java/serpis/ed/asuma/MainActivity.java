ed7f236bad217f80e709102b1f84109356fde305
tsvetos <seve0955@gmail.com>
2018-03-02 09:24:30 +0100

    Añadida aplicacion ASuma

---
+++ ASuma/app/src/main/java/serpis/ed/asuma/MainActivity.java
@@ -0,0 +1,53 @@
+package serpis.ed.asuma;
+
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+
+    }
+    private double valueOne = Double.NaN;
+    private double valueTwo;
+    private static final char ADDITION = '+';
+    private static final char SUBTRACTION = '-';
+    private static final char MULTIPLICATION = '*';
+    private static final char DIVISION = '/';
+
+    private char CURRENT_ACTION;
+    decimalFormat = new DecimalFormat("#.##########");
+    binding.buttonZero.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            binding.editText.setText(binding.editText.getText() + "0");
+        }
+    });
+    private void computeCalculation() {
+        if(!Double.isNaN(valueOne)) {
+            valueTwo = Double.parseDouble(binding.editText.getText().toString());
+            binding.editText.setText(null);
+
+            if(CURRENT_ACTION == ADDITION)
+                valueOne = this.valueOne + valueTwo;
+            else if(CURRENT_ACTION == SUBTRACTION)
+                valueOne = this.valueOne - valueTwo;
+            else if(CURRENT_ACTION == MULTIPLICATION)
+                valueOne = this.valueOne * valueTwo;
+            else if(CURRENT_ACTION == DIVISION)
+                valueOne = this.valueOne / valueTwo;
+        }
+        else {
+            try {
+                valueOne = Double.parseDouble(binding.editText.getText().toString());
+            }
+            catch (Exception e){}
+        }
+    }
+}
